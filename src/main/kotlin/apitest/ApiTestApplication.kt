1eb3a06ec9582e87cc5608e02acfa63cb3d049c6
imollm <ivanmoll07@gmail.com>
2021-10-12 02:49:48 +0200

    Add first endpoint

--- src/main/kotlin/apitest/ApiTestApplication.kt
+++ src/main/kotlin/apitest/ApiTestApplication.kt
@@ -2,6 +2,8 @@ package apitest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class ApiTestApplication
@@ -9,3 +11,15 @@ class ApiTestApplication
 fun main(args: Array<String>) {
     runApplication<ApiTestApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
