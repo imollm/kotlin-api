d03752d9ccfc3d78702254ab6ae868e9ef58a710
imollm <ivanmoll07@gmail.com>
2021-10-12 02:48:37 +0200

    Add project

---
+++ src/main/kotlin/apitest/ApiTestApplication.kt
@@ -0,0 +1,11 @@
+package apitest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiTestApplication>(*args)
+}
